fd2432ea38c514d6f6dfafa26670b913e328d7f5
felipem.medeiros <medeirosfe83@gmail.com>
2023-10-17 10:47:59 -0300

    Initial commit

---
+++ src/test/java/medeiros/felipe/DesenvolvimentoJava2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package medeiros.felipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvimentoJava2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

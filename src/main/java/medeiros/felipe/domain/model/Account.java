0d3907f671dda4db8808040f83941e9f43634bff
felipem.medeiros <medeirosfe83@gmail.com>
2023-10-17 15:48:46 -0300

    hotfix precision & scale

--- src/main/java/medeiros/felipe/domain/model/Account.java
+++ src/main/java/medeiros/felipe/domain/model/Account.java
@@ -13,8 +13,8 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(scale = 13, precision = 2)
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "addiotional_limit",scale = 13, precision = 2)
+    @Column(name = "addiotional_limit",precision = 13, scale = 2)
     private BigDecimal  limit;
 }

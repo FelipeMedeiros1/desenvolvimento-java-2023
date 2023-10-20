db9d8b8a581ecc018b500d4aecc69eaac98cdf76
felipem.medeiros <medeirosfe83@gmail.com>
2023-10-19 22:43:22 -0300

    Criando projeto clínica

---
+++ src/main/java/medeiros/felipe/DesenvolvimentoJava2023Application.java
@@ -0,0 +1,13 @@
+package medeiros.felipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvolvimentoJava2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvolvimentoJava2023Application.class, args);
+	}
+
+}
